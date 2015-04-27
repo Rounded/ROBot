26964f2a2d0b6c669831098c29058d9ced356cb6
Heather Snepenger <hs@roundedco.com>
2015-04-27 15:37:06 -0400

    exposed the urls

--- Pod/Classes/NSManagedObject+CRUD.h
+++ Pod/Classes/NSManagedObject+CRUD.h
@@ -13,6 +13,10 @@
 
 + (NSString *)indexURL;
 + (void)index:(void (^)(void))complete failure:(void (^)(ROBotError *))failure;
+- (NSString *)createURL;
+- (NSString *)readURL;
+- (NSString *)updateURL;
+- (NSString *)deleteURL;
 
 - (void)create:(void (^)(void))complete failure:(void (^)(ROBotError *))failure;
 - (void)read:(void (^)(void))complete failure:(void (^)(ROBotError *))failure;
