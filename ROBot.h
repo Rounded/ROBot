cc0666ee49af7dfd5d328cda9c2e735fd5ac72ef
Heather Snepenger <hs@roundedco.com>
2015-04-27 11:51:29 -0400

    added import header

---
+++ ROBot.h
@@ -0,0 +1,18 @@
+//
+//  ROBot.h
+//  Pods
+//
+//  Created by Heather Snepenger on 4/27/15.
+//
+//
+
+
+#ifndef Pods_ROBot_h
+#define Pods_ROBot_h
+
+#import "ROBotError.h"
+#import "ROBotManagedObject.h"
+#import "ROBotManager.h"
+#import "NSManagedObject+ROSerializer.h"
+
+#endif
