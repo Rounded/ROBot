9df8dd9f07bce819bd57560b08e07d18021d6ac2
Brian Weinreich <be.weinreich@gmail.com>
2015-04-29 21:50:51 -0400

    fixed crud

--- Pod/Classes/NSManagedObject+CRUD.m
+++ Pod/Classes/NSManagedObject+CRUD.m
@@ -54,7 +54,7 @@
         
         BOOL success = false;
         
-        if ([self validateResponseForData:data andResponse:response andError:error]) {
+        if ([NSManagedObject validateResponseForData:data andResponse:response andError:error]) {
             NSError *jsonError = nil;
             NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:&jsonError];
             
@@ -94,7 +94,7 @@
         
         BOOL success = false;
         
-        if ([self validateResponseForData:data andResponse:response andError:error]) {
+        if ([NSManagedObject validateResponseForData:data andResponse:response andError:error]) {
             NSError *jsonError = nil;
             NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:&jsonError];
             
@@ -136,7 +136,7 @@
         
         BOOL success = false;
         
-        if ([self validateResponseForData:data andResponse:response andError:error]) {
+        if ([NSManagedObject validateResponseForData:data andResponse:response andError:error]) {
             NSError *jsonError = nil;
             NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:&jsonError];
             
@@ -176,7 +176,7 @@
     
     [[session dataTaskWithRequest:mutableURLRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
         
-        if ([self validateResponseForData:data andResponse:response andError:error]) {
+        if ([NSManagedObject validateResponseForData:data andResponse:response andError:error]) {
             [self.managedObjectContext deleteObject:self];
             [self saveContext];
             dispatch_async(dispatch_get_main_queue(), ^{
@@ -215,41 +215,50 @@
             NSLog(@"%@",response);
             NSLog(@"%@",error);
         }
-        
-        NSError *jsonError = nil;
-        NSArray *jsonArray = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:&jsonError];
-        
-        if (jsonError != nil) {
-            if ([ROBotManager sharedInstance].verboseLogging == TRUE) {
-                NSLog(@"Could not parse JSON: %@", jsonError.localizedDescription);
-            }
-        } else {
-            // Create the context outside the enumeration block
-            NSManagedObjectContext *context = [NSManagedObjectContext new];
-            context.persistentStoreCoordinator = [[ROBotManager sharedInstance] persistentStoreCoordinator];
-            [jsonArray enumerateObjectsUsingBlock:^(NSDictionary *jsonObject, NSUInteger idx, BOOL *stop) {
-                
-                // Check the database to see if the object in the JSON response exists already (based on the primary key)
-                NSError *error = nil;
-                NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:NSStringFromClass([self class])];
-                [fetchRequest setPredicate:[NSPredicate predicateWithFormat:@"%K = %@", [[self class] primaryKey], jsonObject[[[self class] primaryKey]]]];
-                
-                NSArray *objects = [context executeFetchRequest:fetchRequest error:&error];
-                if (objects.count > 0) {
-                    // The object already exists in the database, so let's just update it
-                    [[objects objectAtIndex:0] setDictionaryToCoreDataEntity:jsonObject];
-                } else {
-                    // The object doesn't exist in the database, so we need to create it
-                    NSManagedObject *newObject = [NSEntityDescription insertNewObjectForEntityForName:NSStringFromClass([self class]) inManagedObjectContext:context];
-                    [newObject setDictionaryToCoreDataEntity:jsonObject];
+
+        if ([self validateResponseForData:data andResponse:response andError:error]) {
+
+            NSError *jsonError = nil;
+            NSArray *jsonArray = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:&jsonError];
+            
+            if (jsonError != nil) {
+                if ([ROBotManager sharedInstance].verboseLogging == TRUE) {
+                    NSLog(@"Could not parse JSON: %@", jsonError.localizedDescription);
                 }
-            }];
-            // Save the context after all the objects have been created
-            [NSManagedObject saveContext:context];
-            if (complete) {
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    complete();
-                });
+            } else {
+                // Create the context outside the enumeration block
+                NSManagedObjectContext *context = [NSManagedObjectContext new];
+                context.persistentStoreCoordinator = [[ROBotManager sharedInstance] persistentStoreCoordinator];
+                [jsonArray enumerateObjectsUsingBlock:^(NSDictionary *jsonObject, NSUInteger idx, BOOL *stop) {
+                    
+                    // Check the database to see if the object in the JSON response exists already (based on the primary key)
+                    NSError *error = nil;
+                    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:NSStringFromClass([self class])];
+                    [fetchRequest setPredicate:[NSPredicate predicateWithFormat:@"%K = %@", [[self class] primaryKey], jsonObject[[[self class] primaryKey]]]];
+                    
+                    NSArray *objects = [context executeFetchRequest:fetchRequest error:&error];
+                    if (objects.count > 0) {
+                        // The object already exists in the database, so let's just update it
+                        [[objects objectAtIndex:0] setDictionaryToCoreDataEntity:jsonObject];
+                    } else {
+                        // The object doesn't exist in the database, so we need to create it
+                        NSManagedObject *newObject = [NSEntityDescription insertNewObjectForEntityForName:NSStringFromClass([self class]) inManagedObjectContext:context];
+                        [newObject setDictionaryToCoreDataEntity:jsonObject];
+                    }
+                }];
+                // Save the context after all the objects have been created
+                [NSManagedObject saveContext:context];
+                if (complete) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        complete();
+                    });
+                }
+            }
+
+        } else {
+            if (failure) {
+                ROBotError *error = [[ROBotError alloc] initWithResponse:response andResponseData:data];
+                failure(error);
             }
         }
     }] resume];
@@ -275,49 +284,57 @@
             NSLog(@"%@",response);
             NSLog(@"%@",error);
         }
-        
-        NSError *jsonError = nil;
-        [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:&jsonError];
-        
-        if (jsonError != nil) {
-            if ([ROBotManager sharedInstance].verboseLogging == TRUE) {
-                NSLog(@"Could not parse JSON: %@", jsonError.localizedDescription);
+
+        if ([self validateResponseForData:data andResponse:response andError:error]) {
+            NSError *jsonError = nil;
+            [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:&jsonError];
+            
+            if (jsonError != nil) {
+                if ([ROBotManager sharedInstance].verboseLogging == TRUE) {
+                    NSLog(@"Could not parse JSON: %@", jsonError.localizedDescription);
+                }
+            } else {
+                NSMutableArray *jsonArray = [NSMutableArray new];
+                if ([[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:&jsonError] isKindOfClass:[NSArray class]]) {
+                    // the response is an array, so set it equal to jsonArray
+                    jsonArray = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:&jsonError];
+                } else {
+                    // if the response is not an array, let's just put it in an array and have it be an array of size 1
+                    [jsonArray addObject:[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:&jsonError]];
+                }
+                
+                [jsonArray enumerateObjectsUsingBlock:^(NSDictionary *jsonObject, NSUInteger idx, BOOL *stop) {
+                    NSManagedObjectContext *context = [NSManagedObjectContext new];
+                    context.persistentStoreCoordinator = [[ROBotManager sharedInstance] persistentStoreCoordinator];
+                    
+                    // Check the database to see if the object in the JSON response exists already (based on the primary key)
+                    NSError *error = nil;
+                    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:NSStringFromClass([self class])];
+                    [fetchRequest setPredicate:[NSPredicate predicateWithFormat:@"%K = %@", @"id", jsonObject[@"id"]]];
+                    
+                    NSArray *objects = [context executeFetchRequest:fetchRequest error:&error];
+                    if (objects.count > 0) {
+                        // The object already exists in the database, so let's just update it
+                        [[objects objectAtIndex:0] saveToDatabase:jsonObject];
+                    } else {
+                        // The object doesn't exist in the database, so we need to create it
+                        NSManagedObject *newObject = [NSEntityDescription insertNewObjectForEntityForName:NSStringFromClass([self class]) inManagedObjectContext:context];
+                        [newObject saveToDatabase:jsonObject];
+                    }
+                }];
+                if (complete) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        complete();
+                    });
+                }
             }
         } else {
-            NSMutableArray *jsonArray = [NSMutableArray new];
-            if ([[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:&jsonError] isKindOfClass:[NSArray class]]) {
-                // the response is an array, so set it equal to jsonArray
-                jsonArray = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:&jsonError];
-            } else {
-                // if the response is not an array, let's just put it in an array and have it be an array of size 1
-                [jsonArray addObject:[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:&jsonError]];
-            }
-            
-            [jsonArray enumerateObjectsUsingBlock:^(NSDictionary *jsonObject, NSUInteger idx, BOOL *stop) {
-                NSManagedObjectContext *context = [NSManagedObjectContext new];
-                context.persistentStoreCoordinator = [[ROBotManager sharedInstance] persistentStoreCoordinator];
-                
-                // Check the database to see if the object in the JSON response exists already (based on the primary key)
-                NSError *error = nil;
-                NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:NSStringFromClass([self class])];
-                [fetchRequest setPredicate:[NSPredicate predicateWithFormat:@"%K = %@", @"id", jsonObject[@"id"]]];
-                
-                NSArray *objects = [context executeFetchRequest:fetchRequest error:&error];
-                if (objects.count > 0) {
-                    // The object already exists in the database, so let's just update it
-                    [[objects objectAtIndex:0] saveToDatabase:jsonObject];
-                } else {
-                    // The object doesn't exist in the database, so we need to create it
-                    NSManagedObject *newObject = [NSEntityDescription insertNewObjectForEntityForName:NSStringFromClass([self class]) inManagedObjectContext:context];
-                    [newObject saveToDatabase:jsonObject];
-                }
-            }];
-            if (complete) {
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    complete();
-                });
+            if (failure) {
+                ROBotError *error = [[ROBotError alloc] initWithResponse:response andResponseData:data];
+                failure(error);
             }
         }
+
         
     }] resume];
 }
@@ -327,7 +344,7 @@
 
 #pragma mark — Helpers
 
-- (BOOL)validateResponseForData:(NSData *)data andResponse:(NSURLResponse *)response andError:(NSError *)error {
++ (BOOL)validateResponseForData:(NSData *)data andResponse:(NSURLResponse *)response andError:(NSError *)error {
     if ([ROBotManager sharedInstance].verboseLogging == TRUE) {
         NSString *responseString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
         NSLog(@"%@",responseString);
